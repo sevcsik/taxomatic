14f3089b032dd71a6b17c451053c8ed58d529aba
Andras Sevcsik <sevcsik@gmail.com>
2014-09-02 00:21:03 +0200

    Moved input-related functions to a module

---
+++ src/App/Input.hs
@@ -0,0 +1,36 @@
+module App.Input (
+	askList,
+	formatList,
+	InvoiceType(Income, Expense)
+) where
+
+import Control.Monad
+import Control.Applicative
+import Data.Maybe
+
+data InvoiceType = Income | Expense deriving (Eq)
+
+readFloat :: IO (Maybe Float)
+readFloat = do
+	value <- getLine
+	if null value
+		then return Nothing
+		else return $ Just (read value :: Float)
+
+ask :: InvoiceType -> IO (Maybe Float)
+ask invoiceType = do
+	if invoiceType == Income
+		then putStrLn $ "Please type the total value of the next income"
+		else putStrLn $ "Please type the total value of the next expense"
+	readFloat
+
+askList :: InvoiceType -> IO [Float]
+askList invoiceType = do
+	result <- ask invoiceType
+	if result /= Nothing
+		then (:) <$> (return (fromJust result)) <*> (askList invoiceType)
+		else return []
+
+formatList :: [Float] -> String
+formatList (x:[]) = show x
+formatList (x:xs) = (show x) ++ ", " ++ (formatList xs)
--- src/main.hs
+++ src/main.hs
@@ -1,36 +1,4 @@
-import Control.Monad
-import Control.Applicative
-import Data.Maybe
-
-import App.Input
-
-readFloat :: IO (Maybe Float)
-readFloat = do
-	value <- getLine
-	if null value
-		then return Nothing
-		else return $ Just (read value :: Float)
-
-data InvoiceType = Income | Expense deriving (Eq)
-
-ask :: InvoiceType -> IO (Maybe Float)
-ask invoiceType = do
-	if invoiceType == Income
-		then putStrLn $ "Please type the total value of the next income"
-		else putStrLn $ "Please type the total value of the next expense"
-	readFloat
-
-askList :: InvoiceType -> IO [Float]
-askList invoiceType = do
-	result <- ask invoiceType
-	if result /= Nothing
-		then (:) <$> (return (fromJust result)) <*> (askList invoiceType)
-		else return []
-
-formatList :: [Float] -> String
-formatList (x:[]) = show x
-formatList (x:xs) = (show x) ++ ", " ++ (formatList xs)
-	
+import App.Input 
 
 main = do
 	putStrLn "Type in incomes"
