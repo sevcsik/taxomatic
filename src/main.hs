cd16b3f372a136a54cf5fff1ae65fb736e406114
Andras Sevcsik <sevcsik@gmail.com>
2014-09-02 00:00:59 +0200

    Finally data input!

--- src/main.hs
+++ src/main.hs
@@ -1,17 +1,40 @@
 import Control.Monad
+import Control.Applicative
+import Data.Maybe
 
-inputIncome :: Int -> IO (Maybe Float)
+import App.Input
 
-inputIncome index = do
-	putStrLn $ "Please type the total value of income #" ++ show index
+readFloat :: IO (Maybe Float)
+readFloat = do
 	value <- getLine
 	if null value
 		then return Nothing
 		else return $ Just (read value :: Float)
 
-main = do
-	result <- inputIncome 1
-	print result
-		
-		
+data InvoiceType = Income | Expense deriving (Eq)
+
+ask :: InvoiceType -> IO (Maybe Float)
+ask invoiceType = do
+	if invoiceType == Income
+		then putStrLn $ "Please type the total value of the next income"
+		else putStrLn $ "Please type the total value of the next expense"
+	readFloat
+
+askList :: InvoiceType -> IO [Float]
+askList invoiceType = do
+	result <- ask invoiceType
+	if result /= Nothing
+		then (:) <$> (return (fromJust result)) <*> (askList invoiceType)
+		else return []
+
+formatList :: [Float] -> String
+formatList (x:[]) = show x
+formatList (x:xs) = (show x) ++ ", " ++ (formatList xs)
 	
+
+main = do
+	putStrLn "Type in incomes"
+	incomes <- askList Income
+	putStrLn $ "Entered incomes: " ++ (formatList incomes)
+	expenses <- askList Expense
+	putStrLn $ "Entered expenses " ++ (formatList expenses)
